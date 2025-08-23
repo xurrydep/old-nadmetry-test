bbf88c7e8c114831614fae6f1d00fb1f08bcf35e
xurry <143620421+xurrydep@users.noreply.github.com>
2025-08-23 18:27:34 +0300

    Add CSP headers and fix missing logo file

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,20 @@ const nextConfig: NextConfig = {
     WALLET_PRIVATE_KEY: process.env.WALLET_PRIVATE_KEY || '4aacc37022413f27533999ac24b269c9a6af38350ca4c9863c87bdab53519e1f',
     NEXT_PUBLIC_PRIVY_APP_ID: process.env.NEXT_PUBLIC_PRIVY_APP_ID || 'cmd8euall0037le0my79qpz42',
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' http://localhost:3000 https://localhost:3000 https://monad-games-id-requestor-app.vercel.app https://www.molandak.net https://www.monad-games-id-requestor-app.vercel.app https://molandak.net https://privy.molandak.net https://auth.privy.io https://verify.walletconnect.com https://verify.walletconnect.org;",
+          },
+        ],
+      },
+    ];
+  },
 };
+
 
 export default nextConfig;
